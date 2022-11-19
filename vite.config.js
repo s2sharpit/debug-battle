a5d9a641b05ab4c1608c41ffaafdeb88786cc3d1
Tushar Saini <s2sharpit@gmail.com>
2022-11-19 18:12:05 +0530

    fixes bugs file path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/debug-battle/',
+  base: '/innovision/',
   build: {
     rollupOptions: {
       input: {
